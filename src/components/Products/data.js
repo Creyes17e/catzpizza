ce9f6006a14b358631c8aab4b4c198334a71c363
Carina Reyes <cereyes1792@gmail.com>
2021-01-29 17:40:17 -0600

    Added data, displayed data, styled products component

---
+++ src/components/Products/data.js
@@ -0,0 +1,36 @@
+import product1 from "../../images/product-1.jpg";
+import product2 from "../../images/product-2.jpg";
+import product3 from "../../images/product-3.jpg";
+
+export const productData = [
+  {
+    img: product1,
+    alt: "Pizza",
+    name: "The Felix",
+    type: "Supreme Pizza",
+    desc:
+      "Marinara, Mozarella, Italian Sausage, Basil, Onions, Bell Peppers, Roma Tomatoes, & Olives",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product2,
+    alt: "Pizza",
+    name: "The Garfield",
+    type: "Lasagna Pizza",
+    desc:
+      "Marinara,  Mozarella, Provolone, Ricotta, Sweet Italian Sausage, Basil, Roma Tomatoes, Parsley, & Oregano",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product3,
+    alt: "Pizza",
+    name: "The Gary",
+    type: "Escargot Pizza",
+    desc:
+      "Pesto, Mozarella, Gruyere, Snails Sauteed w/ Garlic, Shallots, & Butter, Baby Arugula, Onions, & Mushrooms",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+];
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,7 +1,38 @@
 import React from "react";
+import {
+  ProductsContainer,
+  ProductsWrapper,
+  ProductTitle,
+  ProductsHeading,
+  ProductPrice,
+  ProductButton,
+  ProductDesc,
+  ProductCard,
+  ProductImg,
+  ProductInfo,
+} from "./ProductsElements";
 
-const Products = () => {
-  return <div></div>;
+const Products = ({ heading, data }) => {
+  return (
+    <ProductsContainer>
+      <ProductsHeading>{heading}</ProductsHeading>
+      <ProductsWrapper>
+        {data.map((product, index) => {
+          return (
+            <ProductCard key={index}>
+              <ProductImg src={product.img} alt={product.alt} />
+              <ProductInfo>
+                <ProductTitle>{product.name}</ProductTitle>
+                <ProductDesc>{product.desc}</ProductDesc>
+                <ProductPrice>{product.price}</ProductPrice>
+                <ProductButton>{product.button}</ProductButton>
+              </ProductInfo>
+            </ProductCard>
+          );
+        })}
+      </ProductsWrapper>
+    </ProductsContainer>
+  );
 };
 
 export default Products;
