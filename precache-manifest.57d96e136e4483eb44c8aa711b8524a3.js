40d9aaba3edaf2fed990f6aeb46166ab8076f7c9
Carina Reyes <cereyes1792@gmail.com>
2021-04-04 20:34:54 -0500

    Updates

---
+++ precache-manifest.57d96e136e4483eb44c8aa711b8524a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e127230fa3e8803b4ed2faea8549b7f9",
+    "url": "/catzpizza/index.html"
+  },
+  {
+    "revision": "857d95ca93e45547cf23",
+    "url": "/catzpizza/static/js/2.a62c1727.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/catzpizza/static/js/2.a62c1727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa1e8425c871ecb1890",
+    "url": "/catzpizza/static/js/main.e603d807.chunk.js"
+  },
+  {
+    "revision": "6fd5828c86e156d633ba",
+    "url": "/catzpizza/static/js/runtime-main.b3f4545a.js"
+  },
+  {
+    "revision": "665dfac5e24e028feab0aa3e83104590",
+    "url": "/catzpizza/static/media/featured3.665dfac5.jpg"
+  },
+  {
+    "revision": "ccad5f017c9f27c3472f658619281f2c",
+    "url": "/catzpizza/static/media/pizza-3.ccad5f01.jpg"
+  },
+  {
+    "revision": "11ddeb6295d9d8d84d0f4100bb1834bd",
+    "url": "/catzpizza/static/media/product-1.11ddeb62.jpg"
+  },
+  {
+    "revision": "c61c9f8caa578f4ec228f7f28502ac4c",
+    "url": "/catzpizza/static/media/product-2.c61c9f8c.jpg"
+  },
+  {
+    "revision": "40b29cf03c25d898fa0418bc5834e67d",
+    "url": "/catzpizza/static/media/product-3.40b29cf0.jpg"
+  }
+]);
